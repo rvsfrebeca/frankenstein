927e01e4adeb6a2ed0c36cbc0bf32f74a867bb56
Rebeca Ferreira <rvsfrebeca@gmail.com>
2023-06-28 18:28:23 -0300

    initial commit

---
+++ src/main/java/com/ferreira/rebeca/frankinstain/FrankinstainApplication.java
@@ -0,0 +1,13 @@
+package com.ferreira.rebeca.frankinstain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrankinstainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrankinstainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferreira/rebeca/frankinstain/FrankinstainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferreira.rebeca.frankinstain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrankinstainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
